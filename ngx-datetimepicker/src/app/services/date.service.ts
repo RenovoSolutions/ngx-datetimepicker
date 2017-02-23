cb30577257d59acf872699bae0287d0aff254134
theoriginaljosh <theoriginaljosh@gmail.com>
2017-02-23 15:47:33 -0500

    overall refinements and additions to the date service.
    adding get months, years. etc.

--- ngx-datetimepicker/src/app/services/date.service.ts
+++ ngx-datetimepicker/src/app/services/date.service.ts
@@ -4,14 +4,17 @@ export interface dayOfTheMonth {
 	day: number;
 	dayOfTheWeek: number;
 	month: number;
+	date: Date;
 }
+
 
 @Injectable()
 export class DateService {
+	private months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 	constructor() { }
 
-	getDateList(Month: number, Year: number): Array<dayOfTheMonth> {
+	getCurrentMonthDays(Month: number, Year: number): dayOfTheMonth[] {
 		let dayOfTheMonth = new Date(Month + '/1/' + Year);
 		let nextMonth = new Date(Month + '/1/' + Year);
 
@@ -20,33 +23,39 @@ export class DateService {
 		nextMonth.setMonth(nextMonth.getMonth() + 1);
 
 		while (dayOfTheMonth.getMonth() != nextMonth.getMonth()) {
-			returnedDays.push({
+			const dayToAdd = {
 				day: dayOfTheMonth.getDate(),
 				dayOfTheWeek: dayOfTheMonth.getDay(),
-				month: dayOfTheMonth.getMonth() + 1
-			});
+				month: dayOfTheMonth.getMonth() + 1,
+				date: new Date((dayOfTheMonth.getMonth() + 1) + '/' +  dayOfTheMonth.getDate() + '/' + dayOfTheMonth.getFullYear())
+
+			};
+			returnedDays.push(dayToAdd);
 			dayOfTheMonth.setDate(dayOfTheMonth.getDate() + 1);
 		}
+		return returnedDays;
+	}
+
+	getDateList(Month: number, Year: number){
 		return [...this.getPreviousMonthDays(Month, Year),
-		...returnedDays,
+		...this.getCurrentMonthDays(Month, Year),
 		...this.getNextMonthDays(Month, Year)];
 	}
 
 
-	getPreviousMonthDays(Month: number, Year: number): Array<dayOfTheMonth> {
-		let currentMonth = new Date(Month + '/1/' + Year);
+	getPreviousMonthDays(Month: number, Year: number): dayOfTheMonth[] {
+		let day = new Date(Month + '/1/' + Year);
 		let returnedDays: dayOfTheMonth[] = [];
 
-		let dayOfTheWeek = currentMonth.getDay();
-		let day = currentMonth;
+		let dayOfTheWeek = day.getDay();
 
 		while (dayOfTheWeek != 0) {
 			day.setDate(day.getDate() - 1);
 			returnedDays = [{
 				day: day.getDate(),
 				dayOfTheWeek: day.getDay(),
-				month: day.getMonth() + 1
-
+				month: day.getMonth() + 1,
+				date: new Date((day.getMonth() + 1) + '/' +  day.getDate() + '/' + day.getFullYear())
 			}, ...returnedDays];
 			dayOfTheWeek = day.getDay();
 		}
@@ -54,15 +63,14 @@ export class DateService {
 		return returnedDays;
 	}
 
-	getNextMonthDays(Month: number, Year: number): Array<dayOfTheMonth> {
-		let currentMonth = new Date(Month + '/1/' + Year);
-		currentMonth.setMonth(currentMonth.getMonth() + 1);
-		currentMonth.setDate(currentMonth.getDate() - 1);
+	getNextMonthDays(Month: number, Year: number): dayOfTheMonth[] {
+		let day = new Date(Month + '/1/' + Year);
+		day.setMonth(day.getMonth() + 1);
+		day.setDate(day.getDate() - 1);
 
 		let returnedDays: dayOfTheMonth[] = [];
 
-		let dayOfTheWeek = currentMonth.getDay();
-		let day = currentMonth;
+		let dayOfTheWeek = day.getDay();
 
 		while (dayOfTheWeek != 6) {
 			day.setDate(day.getDate() + 1);
@@ -70,7 +78,8 @@ export class DateService {
 			returnedDays = [...returnedDays, {
 				day: day.getDate(),
 				dayOfTheWeek: day.getDay(),
-				month: day.getMonth() + 1
+				month: day.getMonth() + 1,
+				date: new Date((day.getMonth() + 1) + '/' +  day.getDate() + '/' + day.getFullYear())
 			}];
 			dayOfTheWeek = day.getDay();
 		}
@@ -78,9 +87,23 @@ export class DateService {
 		return returnedDays;
 	}
 
+	getMonths():string[]{
+		return this.months;
+	}
 
 	getMonthText(date: Date): string{
-		const monthTextList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		return monthTextList[date.getMonth()];
+		return this.months[date.getMonth()];
+	}
+
+	getAvailableYears(): number[] {
+		const currentYear = new Date().getFullYear();
+		let startYear = currentYear - 80;
+		let returnYears: number[] = [];
+
+		while(startYear <= (currentYear+5)){
+			returnYears.push(startYear);
+			startYear = startYear + 1;
+		}
+		return returnYears;
 	}
 }
