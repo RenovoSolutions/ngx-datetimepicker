e125354dd4c1af38570ea34f1cf9d0bd69fe6c5b
theoriginaljosh <theoriginaljosh@gmail.com>
2017-02-23 15:46:55 -0500

    Date picker component

---
+++ ngx-datetimepicker/src/app/datePicker.component/datePicker.component.spec.ts
@@ -0,0 +1,14 @@
+import { DatePickerComponent } from './datePicker.component';
+
+describe('a input component', () => {
+	let inputComponent : DatePickerComponent;
+
+	// register all needed dependencies
+	beforeEach(() => {
+		inputComponent = new DatePickerComponent();
+	});
+
+	it('should have an instance', () => {
+		expect(inputComponent).toBeTruthy();
+	});
+});
---
+++ ngx-datetimepicker/src/app/datePicker.component/datePicker.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { isMobile } from '../services/isMobile.service';
+import { DateService, dayOfTheMonth } from '../services/date.service';
+
+@Component({
+	selector: 'ngx-datepicker',
+	template: require('./datePicker.component.html')
+})
+
+export class DatePickerComponent implements OnInit {
+    @Input() selectedDate: Date;
+    @Output() selectedDateChange = new EventEmitter<Date>();
+
+	get formattedDate(){
+		if(!this.selectedDate){
+			return '';
+		}
+		return (this.selectedDate.getMonth() + 1) + '/' + this.selectedDate.getDate() + '/' + this.selectedDate.getFullYear();
+	}
+
+	constructor(public isMobile: isMobile, public dateService: DateService) { }
+
+	ngOnInit() {
+        //If no date is selected then default to today's date.
+        if (!this.selectedDate) {
+            this.selectedDate = new Date();
+        }
+        if (typeof this.selectedDate == 'string') {
+            this.selectedDate = new Date(this.selectedDate);
+        }
+	}
+
+}
