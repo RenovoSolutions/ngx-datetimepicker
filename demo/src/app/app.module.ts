88cc6e0653c9a15dd3d20d530f77daa236f33435
theoriginaljosh <theoriginaljosh@gmail.com>
2017-02-24 13:50:30 -0500

    demo project.
    I'm using this to test the validy of the npm install, and visually see that the package works

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import { DateTimePickerModule } from 'ngx-datetimepicker';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    DateTimePickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
