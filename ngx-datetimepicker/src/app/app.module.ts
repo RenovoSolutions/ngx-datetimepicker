5eba4a301cffbb243469dda52a665c6353ffb111
theoriginaljosh <theoriginaljosh@gmail.com>
2017-02-24 10:51:36 -0500

    moving components to datepicker module

--- ngx-datetimepicker/src/app/app.module.ts
+++ ngx-datetimepicker/src/app/app.module.ts
@@ -4,26 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { DateComponent } from './date.component/date.component';
-import { DatePickerComponent } from './datePicker.component/datePicker.component';
+
 
 import { IS_MOBILE_PROVIDER } from './services/isMobile.service';
 import { DateService } from './services/date.service';
 
+import { DateTimePickerModule } from './datetimepicker.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DateComponent,
-    DatePickerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DateTimePickerModule
   ],
   providers: [
-    IS_MOBILE_PROVIDER,
     DateService
   ],
   bootstrap: [AppComponent]
