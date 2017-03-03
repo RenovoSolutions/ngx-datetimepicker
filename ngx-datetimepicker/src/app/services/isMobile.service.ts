3530b9491345d9e332aa853708bc92d4610b1b88
theoriginaljosh <theoriginaljosh@gmail.com>
2017-03-03 11:43:37 -0500

    targeting comm

--- ngx-datetimepicker/src/app/services/isMobile.service.ts
+++ ngx-datetimepicker/src/app/services/isMobile.service.ts
@@ -1,11 +1,17 @@
-import * as ismobilejs from 'ismobilejs';
-import { ValueProvider } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-export abstract class isMobile {
-  public any: boolean;
-};
-
-export const IS_MOBILE_PROVIDER: ValueProvider = {
-      provide: isMobile,
-      useValue: ismobilejs
-    }
+@Injectable()
+export class IsMobileService {
+  public isMobile: boolean;
+  constructor() {
+      this.isMobile = !!(window.navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+            || navigator.userAgent.match(/Opera Mini/i)
+            || navigator.userAgent.match(/IEMobile/i));
+  }
+}
