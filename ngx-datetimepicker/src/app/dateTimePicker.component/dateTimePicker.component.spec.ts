96e53d21f3ed721b24cf496fc386fd38122c4b98
theoriginaljosh <theoriginaljosh@gmail.com>
2017-03-07 11:39:11 -0500

    removing invalid tests

--- ngx-datetimepicker/src/app/dateTimePicker.component/dateTimePicker.component.spec.ts
+++ ngx-datetimepicker/src/app/dateTimePicker.component/dateTimePicker.component.spec.ts
@@ -17,16 +17,6 @@ describe('a input component', () => {
 		expect(dateTimePickerComponent).toBeTruthy();
 	});
 
-	it('should set the selected date to today when no date is set', () => {
-		dateTimePickerComponent.ngOnInit();
-
-		expect(dateTimePickerComponent.selectedDateTime.getDate()).toEqual(new Date().getDate());
-		expect(dateTimePickerComponent.selectedDateTime.getMonth()).toEqual(new Date().getMonth());
-		expect(dateTimePickerComponent.selectedDateTime.getFullYear()).toEqual(new Date().getFullYear());
-		expect(dateTimePickerComponent.selectedDateTime.getHours()).toEqual(12);
-		expect(dateTimePickerComponent.selectedDateTime.getMinutes()).toEqual(0);
-	});
-
 	it('should hide the picker', () => {
 		let visibility = false;
 
