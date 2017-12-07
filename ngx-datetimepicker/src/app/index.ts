dd20093e1e3bb3bcc508d08cc020faf45ce8ca60
theoriginaljosh <theoriginaljosh@gmail.com>
2017-12-07 08:24:43 -0500

    adding barrel for exporting components and module

---
+++ ngx-datetimepicker/src/app/index.ts
@@ -0,0 +1,4 @@
+export { DateTimePickerModule } from './datetimepicker.module';
+export { TimePickerComponent } from './timePicker.component/timePicker.component';
+export { DatePickerComponent } from './datePicker.component/datePicker.component';
+export { DateTimePickerComponent } from './dateTimePicker.component/dateTimePicker.component';
