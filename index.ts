3b2da93ab8ba9ad2f354cd89e700e115332c4aa7
theoriginaljosh <theoriginaljosh@gmail.com>
2017-12-07 08:06:48 -0500

    removing old index.js file

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { DateTimePickerModule } from './ngx-datetimepicker/src/app/datetimepicker.module';
-export { TimePickerComponent } from './ngx-datetimepicker/src/app/timePicker.component/timePicker.component';
-export { DatePickerComponent } from './ngx-datetimepicker/src/app/datePicker.component/datePicker.component';
-export { DateTimePickerComponent } from './ngx-datetimepicker/src/app/dateTimePicker.component/dateTimePicker.component';
