9d89dc33b8f244cd6ad592679d84ff89077097cb
Alejandro Cremades <alecrem@users.noreply.github.com>
2019-06-17 13:35:35 +0900

    Minutes always padded to 2 digits when 24 hour mode is on

--- ngx-datetimepicker/src/app/time.component/time.component.ts
+++ ngx-datetimepicker/src/app/time.component/time.component.ts
@@ -50,6 +50,7 @@ export class TimeComponent implements OnInit {
 		}
 		if (this.use24HourClock) {
 			this.hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
+			this.minutes = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
 			this.selectedClock = '';
 		}
 	}
