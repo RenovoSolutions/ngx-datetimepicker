e33c39a32a6a7360053be7272f4ea69aa1915038
Jeremy Quick <jrquick@uab.edu>
2019-11-05 09:26:03 -0600

    Fix lint errors

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	public dateTimeExample = null
-	public dateExample = null
-	public timeExample = null
+  public dateTimeExample = null;
+  public dateExample = null;
+  public timeExample = null;
 
-	title = 'ngx-datetime-picker';
+  title = 'ngx-datetime-picker';
 
-	setToNow(): void{
-		this.dateExample = new Date();
-		this.timeExample = `${this.dateExample.getHours()}:${this.dateExample.getMinutes()} ${(this.dateExample.getHours() > 11 ? 'am' : 'pm')}`;
-		this.dateTimeExample = new Date();
-	}
+  setToNow(): void {
+    this.dateExample = new Date();
+    this.timeExample = `${this.dateExample.getHours()}:${this.dateExample.getMinutes()} ${(this.dateExample.getHours() > 11 ? 'am' : 'pm')}`;
+    this.dateTimeExample = new Date();
+  }
 }
